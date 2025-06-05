e778a2d5bf6ce39fc0d8b79a1358afe825e41aa4
Xiongrw <xiongrw0701@163.com>
2025-06-06 03:16:22 +0800

    main

--- main_db.py
+++ main_db.py
@@ -147,8 +147,10 @@ def main():
             parser_db.set_handle()  # to set the global_parser in common_db.py
 
             try:
-                common_db.global_syn_tree = common_db.global_parser.parse(sql_str.strip(),
-                                                                          lexer=common_db.global_lexer)  # construct the global_syn_tree
+                common_db.global_syn_tree = common_db.global_parser.parse(
+                    sql_str.strip(),
+                    lexer=common_db.global_lexer
+                )  # construct the global_syn_tree
                 #reload(query_plan_db)
                 query_plan_db.construct_logical_tree()
                 query_plan_db.execute_logical_tree()
@@ -163,7 +165,73 @@ def main():
             table_name = input('please input the name of the table to be deleted from:')
             field_name = input('please input the field name and the corresponding keyword (fieldname:keyword):')
             # to the students: to be inserted here, delete the line from data files
+######
+            if isinstance(table_name, str):
+                table_name = table_name.encode('utf-8')
 
+            # 解析输入的字段名和关键字
+            try:
+                field_name, keyword = field_name.split(':')
+                field_name = field_name.strip()
+                keyword = keyword.strip()
+            except:
+                print('Invalid input format! Please use "fieldname:keyword"')
+                choice = input(PROMPT_STR)
+                continue
+
+            # 检查表是否存在
+            if schemaObj.find_table(table_name.strip()):
+                dataObj = storage_db.Storage(table_name)
+                records = dataObj.getRecord()
+                field_list = dataObj.getFieldList()
+
+                # 查找字段在记录中的索引位置
+                field_index = -1
+                for i, field in enumerate(field_list):
+                    if field[0].strip().decode('utf-8') == field_name:
+                        field_index = i
+                        break
+
+                if field_index == -1:
+                    print('Field name not found in table!')
+                else:
+                    # 查找匹配的记录
+                    found = False
+                    new_records = []
+
+                    for record in records:
+                        # 检查记录中对应字段的值是否与关键字匹配
+                        field_value = str(record[field_index])
+                        if field_value.strip() == keyword:
+                            found = True
+                            print(f'Record found and will be deleted: {record}')
+                        else:
+                            new_records.append(record)
+
+                    if found:
+                        # 重建表并重新插入记录（除了要删除的记录）
+                        dataObj.delete_table_data(table_name.strip())
+                        del dataObj
+
+                        # 重新创建表
+                        if schemaObj.find_table(table_name.strip()):
+                            dataObj = storage_db.Storage(table_name)
+
+                            # 重新插入保留的记录
+                            for record in new_records:
+                                record_str = [str(item) for item in record]
+                                dataObj.insert_record(record_str)
+
+                            print('Record deleted successfully!')
+                            del dataObj
+                        else:
+                            print('Error rebuilding table after deletion!')
+                    else:
+                        print('No record found with the specified keyword!')
+                        del dataObj
+            else:
+                print(f'Table {table_name.decode("utf-8")} not found!')
+#####
             choice = input(PROMPT_STR)
 
         elif choice == '7':  # update a line of data given the keyword
@@ -172,7 +240,108 @@ def main():
             field_name = input('please input the field name:')
             field_name_value = input('please input the old value of the field:')
             # to the students: to be inserted here, update the line according to the user input
+#####
+            if isinstance(table_name, str):
+                table_name = table_name.encode('utf-8')
 
+            field_name = field_name.strip()
+            field_name_value = field_name_value.strip()
+
+            # 检查表是否存在
+            if schemaObj.find_table(table_name.strip()):
+                dataObj = storage_db.Storage(table_name)
+                records = dataObj.getRecord()
+                field_list = dataObj.getFieldList()
+
+                # 查找字段在记录中的索引位置
+                field_index = -1
+                for i, field in enumerate(field_list):
+                    if field[0].strip().decode('utf-8') == field_name:
+                        field_index = i
+                        break
+
+                if field_index == -1:
+                    print('Field name not found in table!')
+                else:
+                    # 查找匹配的记录
+                    found = False
+                    new_records = []
+
+                    for record in records:
+                        # 检查记录中对应字段的值是否与关键字匹配
+                        field_value = str(record[field_index])
+                        if field_value.strip() == field_name_value:
+                            found = True
+                            print(f'Record found: {record}')
+                            # 询问用户要更新哪个字段和新值
+                            update_field = input('Input the field name you want to update: ')
+                            update_value = input('Input the new value: ')
+
+                            # 查找更新字段的索引
+                            update_index = -1
+                            for j, f in enumerate(field_list):
+                                if f[0].strip().decode('utf-8') == update_field.strip():
+                                    update_index = j
+                                    break
+
+                            if update_index == -1:
+                                print('Update field name not found!')
+                                new_records.append(record)
+                            else:
+                                # 创建更新后的记录
+                                updated_record = list(record)
+                                # 根据字段类型转换值
+                                field_type = field_list[update_index][1]
+                                if field_type == 2:  # int类型
+                                    try:
+                                        updated_record[update_index] = int(update_value)
+                                    except:
+                                        print('Invalid value for integer field!')
+                                        new_records.append(record)
+                                        continue
+                                elif field_type == 3:  # boolean类型
+                                    try:
+                                        updated_record[update_index] = bool(update_value)
+                                    except:
+                                        print('Invalid value for boolean field!')
+                                        new_records.append(record)
+                                        continue
+                                else:  # 字符串类型
+                                    if len(update_value) > field_list[update_index][2]:
+                                        print('Value too long for the field!')
+                                        new_records.append(record)
+                                        continue
+                                    updated_record[update_index] = update_value
+
+                                print(f'Record will be updated to: {tuple(updated_record)}')
+                                new_records.append(tuple(updated_record))
+                        else:
+                            new_records.append(record)
+
+                    if found:
+                        # 重建表并重新插入更新后的记录
+                        dataObj.delete_table_data(table_name.strip())
+                        del dataObj
+
+                        # 重新创建表
+                        if schemaObj.find_table(table_name.strip()):
+                            dataObj = storage_db.Storage(table_name)
+
+                            # 重新插入记录
+                            for record in new_records:
+                                record_str = [str(item) for item in record]
+                                dataObj.insert_record(record_str)
+
+                            print('Record updated successfully!')
+                            del dataObj
+                        else:
+                            print('Error rebuilding table after update!')
+                    else:
+                        print(f'No record found with the specified value "{field_name_value}" in field "{field_name}"!')
+                        del dataObj
+            else:
+                print(f'Table {table_name.decode("utf-8")} not found!')
+######
             choice = input(PROMPT_STR)
 
 
@@ -187,3 +356,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
